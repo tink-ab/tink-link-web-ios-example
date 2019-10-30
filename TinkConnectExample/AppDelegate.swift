2a479593d1a4eecfe5d39d2a57889454148c7dc1
Lukas Lipka <lukaslipka@gmail.com>
2019-10-30 12:54:01 +0100

    Add handler for error deep link.

--- TinkConnectExample/AppDelegate.swift
+++ TinkConnectExample/AppDelegate.swift
@@ -6,10 +6,20 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey: Any] = [:]) -> Bool {
         if url.host == "authorize" {
-            if let components = URLComponents(url: url, resolvingAgainstBaseURL: true)?.queryItems,
-                let code = components.first(where: { $0.name == "code" })?.value,
-                let state = components.first(where: { $0.name == "state" })?.value {
+            guard let components = URLComponents(url: url, resolvingAgainstBaseURL: true),
+                let queryItems = components.queryItems else {
+                return true
+            }
+
+            if let code = queryItems.first(where: { $0.name == "code" })?.value,
+                let state = queryItems.first(where: { $0.name == "state" })?.value {
                 let notification = AccessCodeReceivedNotification(code: code, state: state)
+                NotificationCenter.default.post(name: notification.name, object: nil, userInfo: notification.userInfo)
+                return true
+            }
+
+            if let error = queryItems.first(where: { $0.name == "error" })?.value, !error.isEmpty {
+                let notification = ErrorReceivedNotification(error: error)
                 NotificationCenter.default.post(name: notification.name, object: nil, userInfo: notification.userInfo)
                 return true
             }
